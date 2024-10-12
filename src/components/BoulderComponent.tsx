8825415e0a096410596e93ac25cf49343ea8e274
Sandra Ramirez Puente <sandra.ramirez@acaitravel.com>
2024-10-12 17:32:34 +0200

    first version done

---
+++ src/components/BoulderComponent.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+
+type Props = {
+  isMoving?: boolean;
+  what: any;
+  soWhat: () => void;
+  when: any;
+};
+
+const BoulderComponent = ({ isMoving, what, soWhat, when }: Props) => {
+  const [xState, setXState] = useState(0);
+  const [yState, setYState] = useState(0);
+  const [rotation, setRotation] = useState(0);
+  const boulderRef = useRef(null);
+
+  useEffect(() => {
+    detectCollision();
+  }, [when]);
+
+  const detectCollision = () => {
+    if (boulderRef.current) {
+      const boulder = (boulderRef.current as any).getBoundingClientRect();
+      const didCollide =
+        boulder.left + 30 < what.right &&
+        boulder.right - 30 > what.left &&
+        boulder.bottom - 30 > what.top &&
+        boulder.top + 30 < what.bottom;
+      if (didCollide) {
+        soWhat();
+      }
+    }
+  };
+  useEffect(() => {
+    setXState(Math.random() * (window.innerWidth - 80));
+    setYState(-Math.random() * 100 - 100);
+    setRotation(Math.random() * 360);
+  }, []);
+
+  return (
+    <div
+      ref={boulderRef}
+      className="boulder-shadow"
+      style={{
+        position: "absolute",
+        left: xState,
+        top: yState,
+        animation: "moveDown 10s linear forwards",
+        animationPlayState: isMoving ? "running" : "paused",
+      }}
+    >
+      <Image
+        src={"/rock.png"}
+        width={80}
+        height={80}
+        alt={"rock"}
+        priority
+        style={{
+          rotate: `${rotation}deg`,
+        }}
+      />
+    </div>
+  );
+};
+
+export default BoulderComponent;
